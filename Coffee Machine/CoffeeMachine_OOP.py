0e4f247490b7909ab76970076debab0effec414d
menriquez1313 <menriquez1313@gmail.com>
2022-07-06 21:29:45 -0400

    Update CoffeeMachine_OOP.py

--- Coffee Machine/CoffeeMachine_OOP.py
+++ Coffee Machine/CoffeeMachine_OOP.py
@@ -10,78 +10,12 @@ from money_machine import MoneyMachine
 import time
 
 #Coffee Menu
-"""
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-"""
+
 
 #These are the limited resources. 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-    "money": 0
-}
 
-"""
-#checks if there is enough resources
-def sufficient(coffee):
-    for items in coffee:
-        if coffee[items] > resources[items]:
-            print("Sorry! Insufficient ingredients!")
-            return False
-    return True
-
-#Checks if the user has enough money
-def money(price):
-    quar = float(input("How many quarters? "))
-    dim = float(input("How many dimes? "))
-    nic = float(input("How many nickels? "))
-    penn = float(input("How many pennies? "))
-    
-    total = round((quar * .25) + (dim * .10) + (nic * .05) + (penn * .01),2)
-    print(f"You have paid is ${total}")
-    print("...")
-    if total < price:
-        print("Insufficient Funds!")
-        return False
-    print(f"Your change is ${round(total - price,2)}")
-    rec_money = resources.get("money")
-    resources["money"] = rec_money + total
-    return True
-
-#updates the resources after payment   
-def make_coffee(ingredients):
-     for items in ingredients:
-        resources[items] = resources[items] - ingredients[items]
-        #print(ingredients[items]) #test to see if resources are updating
- """    
 
 menu = Menu()
-#menu_item = Menu()
 coffee_maker = CoffeeMaker() #resources
 money_maker = MoneyMachine()
 
@@ -96,12 +30,6 @@ while is_on == True:
         print("Have a nice day! ")
         is_on = False
     elif choice == "report":
-        """
-        print(f"Water: {resources['water']}ml.")
-        print(f"Milk: {resources['milk']}ml.")
-        print(f"Coffee: {resources['coffee']}g.")
-        print(f"Money:${resources['money']}")
-        """
         print(coffee_maker.report())
         print(money_maker.report())
     else:
@@ -115,4 +43,5 @@ while is_on == True:
                 print(f"Here is your {choice}!")
         """
         drink = menu.find_drink()
+        print(coffee_maker.is_resource_sufficient(drink))
         
