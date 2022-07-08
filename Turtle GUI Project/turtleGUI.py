1d4dbedde397778ed56abf7149936ecdbd886686
menriquez1313 <menriquez1313@gmail.com>
2022-07-08 16:13:07 -0400

    Create turtleGUI.py
    Testing Turtle GUI

---
+++ Turtle GUI Project/turtleGUI.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+
+#testing Turtle
+the_turtle = Turtle()
+the_turtle.shape("turtle")
+the_turtle.color("blue")
+the_turtle.forward(100)
+the_turtle.right(90)
+the_turtle.forward(100)
+the_turtle.right(90)
+the_turtle.forward(100)
+the_turtle.right(90)
+the_turtle.forward(100)
+the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+input("Press any key to exit...")
+screen = Screen()
+screen.exitonclick 
