dfcf75371115fcbac4a231a184745c597cfe03b8
menriquez1313 <menriquez1313@gmail.com>
2022-07-11 14:06:28 -0400

    Update turtleGUI.py

--- Turtle GUI Project/turtleGUI.py
+++ Turtle GUI Project/turtleGUI.py
@@ -4,7 +4,7 @@ from turtle import Turtle, Screen
 the_turtle = Turtle()
 the_turtle.shape("turtle")
 the_turtle.color("blue")
-the_turtle.forward(100)
+the_turtle.dot()
 the_turtle.right(90)
 the_turtle.forward(100)
 the_turtle.right(90)
@@ -14,10 +14,11 @@ the_turtle.forward(100)
 the_turtle.right(90)
 
 
-
-
-
-
+for x in range(20):
+    the_turtle.forward(10)
+    the_turtle.penup()
+    the_turtle.forward(10)
+    the_turtle.pendown()
 
 
 
