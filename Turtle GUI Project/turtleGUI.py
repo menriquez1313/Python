48c347dbc427cbab6f7e66e7f375fde4a574e767
menriquez1313 <menriquez1313@gmail.com>
2022-07-12 13:43:54 -0400

    Update turtleGUI.py

--- Turtle GUI Project/turtleGUI.py
+++ Turtle GUI Project/turtleGUI.py
@@ -11,36 +11,29 @@ def random_color():
     random_color = (r, g, b)
     return random_color
 
+
+    
 #testing Turtle
 the_turtle = Turtle()
 
-
-#colors =["blue", "red", "orange", "green", "pink", "teal", "purple", "brown"]
 color_choice = 0
 line = 3
 shapes = 8
 degrees = 360
 directions = [0, 90, 180, 270]
 
-# the_turtle.width(10)
-# the_turtle.speed(10)
-# for x in range(shapes):
-#     the_turtle.color(random.choice(colors))
-#     for y in range(line):
-#         the_turtle.right(degrees/line)
-#         the_turtle.forward(100)
-#     line += 1
-#     color_choice += 1
+#spirograph
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        #the_turtle.width(20)
+        the_turtle.speed("fastest") 
+        the_turtle.color(random_color())
+        the_turtle.circle(100)
+        the_turtle.seth(the_turtle.heading() + size_of_gap)
+        
+   
+draw_spirograph(5) 
 
-#random walk
-while True:
-    the_turtle.width(20)
-    the_turtle.speed(10)
-    the_turtle.color(random_color())
-    the_turtle.forward(50)
-    the_turtle.seth(random.choice(directions))
-    
-
-# input("Press any key to exit...")
-# screen = Screen()
-# screen.exitonclick 
+input("Press any key to exit...")
+screen = Screen()
+screen.exitonclick 
