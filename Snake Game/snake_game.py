ad3830f58168828eb3881b3cd25d9c28e460390c
menriquez1313 <menriquez1313@gmail.com>
2022-07-15 14:08:49 -0400

    Create snake_game.py

---
+++ Snake Game/snake_game.py
@@ -0,0 +1,44 @@
+from tracemalloc import start
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+snake = Turtle()
+snake.shape("square")
+snake.color("white")
+
+starting_positions = [(0,0), (-20,0), (-40,0)]
+segments =[]
+    
+for position in starting_positions:
+    new_seg = Turtle("square")
+    new_seg.color("white")
+    new_seg.goto(position)
+    segments.append(new_seg)
+ 
+game_on = True
+
+ 
+while game_on:
+    
+    for snake in segments:
+    
+        snake.forward(1)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
