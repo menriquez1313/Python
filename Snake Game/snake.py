6506e018d9ce9ff5eb9ed704f000db87cfe95dc5
menriquez1313 <menriquez1313@gmail.com>
2022-07-19 15:46:36 -0400

    Turning Update
    When turning, the snake CANNOT turn its opposite way.

--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -1,12 +1,17 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
-MOVE_DISTANCE =20
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     
     def __init__(self):
         self.segment = []
         self.create_snake()
+        self.head = self.segment[0]
         
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -22,16 +27,20 @@ class Snake:
             new_x = self.segment[seg_num - 1].xcor()
             new_y = self.segment[seg_num - 1].ycor()
             self.segment[seg_num].goto(new_x, new_y)
-        self.segment[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
         
     def up(self):
-        self.segment[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.segment[0].setheading(UP)
         
     def down(self):
-        self.segment[0].setheading(-90)
+        if self.head.heading() != UP:
+            self.segment[0].setheading(DOWN)
     
     def left(self):
-        self.segment[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.segment[0].setheading(LEFT)
     
     def right(self):
-        self.segment[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.segment[0].setheading(RIGHT)
