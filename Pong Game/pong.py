902a2fae768310f76b07a627cfba0e2295137d12
menriquez1313 <menriquez1313@gmail.com>
2022-07-26 17:59:14 -0400

    Updated: 2 Player Paddles
    +Player 1 can use "w" and "s" keys.
    +Player 2 can use "Up" and  "Down" keys.
    
    +Changed paddles to a class.

--- Pong Game/pong.py
+++ Pong Game/pong.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle, width 
+from paddle import Paddle
 
 screen = Screen()
 screen. bgcolor("black")
@@ -6,26 +7,18 @@ screen.setup(width=800, height=600)
 screen.title("PONG")
 screen.tracer(0) #turns off animation
 
-paddle = Turtle()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-#User paddle is created
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(x=350, y=0)
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
 
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+
     
 screen.listen()
-screen.onkey(go_up,"Up")
-screen.onkey(go_down,"Down")
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
 
 game_on = True
 while game_on:
