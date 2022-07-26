8fc5055993f9b3c1b40d012d337b1cb92035df33
menriquez1313 <menriquez1313@gmail.com>
2022-07-26 18:22:55 -0400

    Updated Ball
    +Added ball
    +Added border collision
    +Added the ball to bounce off Top and Bottom borders

--- Pong Game/pong.py
+++ Pong Game/pong.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle, width 
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen. bgcolor("black")
@@ -9,8 +11,7 @@ screen.tracer(0) #turns off animation
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
-
+ball = Ball()
 
 
     
@@ -22,6 +23,13 @@ screen.onkey(l_paddle.go_down,"s")
 
 game_on = True
 while game_on:
+    time.sleep(0.1)
     screen.update() #updates screen after animation is turned off. 
+    ball.move()
     
+    #Detect collision with wall 
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        #needs to bounce
+        ball.bounce()
+        
 screen.exitonclick()
