42dcdf9813410fd464bf6b61b2a69b4c0f220bc2
menriquez1313 <menriquez1313@gmail.com>
2022-08-01 14:11:17 -0400

    Update: Finished
    +Added Level Up Text
    +Added Game Over Text when Turtle collides with Car

--- Crossing Turtle Game/main.py
+++ Crossing Turtle Game/main.py
@@ -11,6 +11,7 @@ screen.tracer(0)
 #Turtle and Car
 player = Player()
 car_manage = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up,"Up")
@@ -28,13 +29,13 @@ while game_is_on:
     for car in car_manage.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
             
     #Detects successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manage.level_up()
-
-
+        scoreboard.increase_level()
 
 
 screen.exitonclick()
