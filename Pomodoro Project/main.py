074b5ac04c28c8d5e7b478b3ea83b2deb4bce411
menriquez1313 <menriquez1313@gmail.com>
2022-08-16 18:28:09 -0400

    Update
    +Start Timer works

--- Pomodoro Project/main.py
+++ Pomodoro Project/main.py
@@ -1,7 +1,8 @@
-from cgitb import reset
+from cgitb import reset, text
 from tkinter import *
 from tkinter.tix import COLUMN
 from turtle import color
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,25 +16,39 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    countdown(WORK_MIN*60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+def countdown(count):
+    #"00:00"
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    
+    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100,pady=50, bg=YELLOW)
 
+
+    
+
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1,row=1)
+
+
 
 timer_label = Label(text="TIMER",fg=GREEN, font=(FONT_NAME, 35, "bold"))
 timer_label.config(bg=YELLOW)
 timer_label.grid(column=1, row=0)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start",command=start_timer)
 start_button.grid(column=0,row=2)
 
 reset_button = Button(text="Reset")
@@ -41,4 +56,6 @@ reset_button.grid(column=2, row=2)
 
 checkmark_button = Button(text= "✔", fg=GREEN)
 checkmark_button.grid(column=1, row=3)
+
+
 window.mainloop()
