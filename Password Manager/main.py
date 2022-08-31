5d41d19896a0659c3db1c060c3bc5f769eb145ae
menriquez1313 <81117223+menriquez1313@users.noreply.github.com>
2022-08-31 16:49:14 -0700

    Finished!
    +Website, Password, and Email can be saved
    +Password Generator works
    +Generated Password is automatically copied onto clipboard

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,14 +1,63 @@
+from email import message
 from email.mime import image
 from tkinter import *
+from tkinter import messagebox 
+from random import choice, randint, shuffle
+from turtle import title
+import pyperclip
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    password_entry.delete(0,END) #clears password entry if regenerating a new password
+    
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    
+    password_list = password_letters + password_numbers + password_symbols
+
+    password_list += choice(numbers)
+
+    shuffle(password_list)
+
+    # password = ""
+    # for char in password_list:
+    # password += char
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+    
+    messagebox.showinfo(title="Password Copied", message="Password has been clipped!")
+    
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def save():
+    
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="OOPS", message="Please fill in the blank fields. \nThank You!")
+    
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details enter: \nEmail: {email} \nPassword: {password} \nIs it ok to save? ")
+        
+        if is_ok == TRUE:
+            with open("data.txt","a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+        
+   
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20,pady=20)
+window.config(padx=50,pady=50)
 
 canvas = Canvas(height=200,width=200)
 logo_img = PhotoImage(file="logo.png")
@@ -16,24 +65,26 @@ canvas.create_image(100,100,image=logo_img)
 canvas.grid(row=0, column=1)
 
 #Labels
-website_label = Label(text="Website: ")
+website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-email_label = Label(text="Email/Username: ")
+email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
-password_label = Label(text="Password: ")
+password_label = Label(text="Password:")
 password_label.grid(column=0,row=3)
 
 #Entries
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
-password_entry = Entry(width=21)
+email_entry.insert(0, "hero@email.com")
+password_entry = Entry(width=20)
 password_entry.grid(column=1, row=3)
 
 #button 
-generate_password = Button(text="Generate Password")
+generate_password = Button(text="Generate Password",command=generate_password)
 generate_password.grid(column=2, row=3)
-add = Button(text="Add", width=36)
+add = Button(text="Add", width=35, command=save)
 add.grid(column=1, row=4, columnspan=2)
 window.mainloop()
