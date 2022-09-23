1f732617f1587ef4c0bdb5635c1bbc933fa338b1
menriquez1313 <81117223+menriquez1313@users.noreply.github.com>
2022-09-23 14:47:29 -0700

    Update
    +English translation flips after 3 seconds.

--- Flashcard Project/main.py
+++ Flashcard Project/main.py
@@ -11,18 +11,31 @@ to_learn = data.to_dict(orient="records") #
 #print(to_learn)
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    print(current_card["French"])
+    #print(current_card["English"])
     canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
     
+
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
-card_front_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_image)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="word",font=("Ariel", 60,"bold"))
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)#fills in background color
